6ad24867d1490d23b889ce2bab0a891077fa7294
VincentHuaa <77996381+VincentHuaa@users.noreply.github.com>
2021-02-02 13:55:56 -0500

    Completer

--- exercice.py
+++ exercice.py
@@ -3,23 +3,51 @@
 
 
 def is_even_len(string: str) -> bool:
-    pass
+    if len(string) % 2 == 0:
+        return True
+
+    return False
 
 
 def remove_third_char(string: str) -> str:
-    pass
+    return string[:2] + string [3:]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    pass
+    for i in range(len(string)):
+        if string[i] == old_char:
+            string = string[:i] + new_char + string[i +1:]
+
+    return string
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    pass
+    occurence = 0
+    for c in string:
+        if c == char:
+            occurence += 1
+
+    return occurence
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    pass
+    ''' #Non fonctionnel pour mots contenant dans un autre mot
+    no_of_word = 0
+    for i in range(len(sentence)):
+        if sentence [i:i + len(word)] == word
+            nb_of_word += 1
+
+    return nb_of_word
+    '''
+
+    number_of_word = 0      #Non fonctionnel pour ponctuation
+    sentence = sentence.split()
+
+    for i in sentence:
+        if i == word:
+            number_of_word += 1
+
+    return number_of_word
 
 
 def main() -> None:
@@ -35,7 +63,7 @@ def main() -> None:
     chaine = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_number_of_char(chaine, 'l')}")
+    print(f"Le nombre d'occurrence de l dans hello world est : {get_number_of_char(chaine, 'l')}")
     
     chaine = "Baby shark doo doo doo doo doo doo"
     print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
